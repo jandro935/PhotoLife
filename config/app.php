38c8940c4b9d15fbbf1dee013708a91deb5ef8fc
jandro935 <jandro935@gmail.com>
2017-06-14 13:47:57 +0200

    Assets Folder Added

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
         /*
          * Package Service Providers...
          */
+        AdamWathan\EloquentOAuthL5\EloquentOAuthServiceProvider::class,
         Laravel\Tinker\TinkerServiceProvider::class,
 
         /*
@@ -226,6 +227,7 @@ return [
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
 
+        'SocialAuth' => AdamWathan\EloquentOAuth\Facades\OAuth::class,
     ],
 
 ];
